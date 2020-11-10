c5f93cb739b08a600f8e2b2e64330d8c41f2ce9f
Audy Daniels <audy.daniels@gmail.com>
2020-11-10 15:00:50 -0600

    added output varable

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
 provider "aws" {
   region = "us-east-2"
 }
+variable "server_port" {
+  description = "The port the server will use for HTTP requestes"
+  default     = 8080
+}
 
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
+}
 resource "aws_instance" "example" {
   ami 		= "ami-0a91cd140a1fc148a"
   instance_type = "t2.micro"
@@ -9,10 +16,10 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
 	      #!/bin/bash
 	      echo "HEllo, World"> index.html
-     	      nohup busybox httpd -f -p 8080 &
+     	      nohup busybox httpd -f -p "${var.server_port}" &
               EOF
   tags = {
-    name = "terraform-example.2.2"
+    name = "terraform-example-version2"
   }
 }
 
@@ -20,8 +27,8 @@ resource "aws_security_group" "instance" {
   name = "terraform-example"
   
   ingress {
-    from_port	 = 8080
-    to_port      = 8080
+    from_port	 = "${var.server_port}"
+    to_port      = "${var.server_port}"
     protocol  	 = "tcp"
     cidr_blocks = ["108.90.7.137/32"]
   } 
